8cedcc1dc9d04e34dc34874a6bf37fc9d5d1b0d3
unknown <SQLI@SQLI-ASABIRI.SQLI>
2019-03-01 10:45:17 +0100

    Implementation du service

--- src/Services/DAOInterface.php
+++ src/Services/DAOInterface.php
@@ -5,7 +5,7 @@ interface DAOInterface {
     //Select
     public function findPropertyAll();
     public function findPropertyLatest();
-    public function findPropertyByName($name);
+    public function findPropertyByTitle($name);
     public function findPropertyByPriceRange($minPrice, $maxPrice);
     public function findPropertyByDescription($description);
     public function findPropertyById($id);
